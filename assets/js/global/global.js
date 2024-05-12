5024c9094a1b126539dc8944e21ca4acd27a0520
Ivan Martins <ivanmartins.ilha@gmail.com>
2024-05-12 17:13:08 -0300

    feat:create section mobile and letle animation

--- assets/js/global/global.js
+++ assets/js/global/global.js
@@ -1,3 +1,11 @@
+const score = document.querySelector(".numero5 h2")
+const score6 = document.querySelector(".numero6 h2")
+const score10 = document.querySelector(".numero10 h2")
+let counter = 0;
+let counter6 = 0;
+let counter10 = 0;
+
+
 (function (){
 
   // Bloqueando arrasta imagens do site para baixar
@@ -14,4 +22,34 @@
 
 const apresentacao = document.querySelector('.apresentacao')
 
-console.log(apresentacao.children);
+const counterNumebers = (counter, limit, element) =>{
+  const timer = setInterval(() =>{
+    if(counter === limit){
+      clearInterval(timer)
+    }
+    element.textContent = `${counter}+ `
+    counter++
+  }, 300)
+}
+
+counterNumebers(0, 5, score)
+counterNumebers(0, 6, score6)
+counterNumebers(0, 10, score10)
+
+// const isCounter6 = setInterval(() =>{
+//   console.log('rodando...')
+//   if(counter6 === 6){
+//     clearInterval(isCounter6)
+//   }
+//   score6.textContent = `${counter6}+`
+//   counter6++
+// }, 300)
+
+// const timer10 = setInterval(() =>{
+//   console.log('rodando...')
+//   if(counter10 === 10){
+//     clearInterval(timer10)
+//   }
+//   score10.textContent = `${counter10}+`
+//   counter10++
+// }, 300)
